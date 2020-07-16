a79874eb108b1747ece02197b9a94188b6232c2b
Valeriy Danets <dantesvv@gmail.com>
2020-07-17 00:01:41 +0300

    added posibility to upload image

--- client/src/app/components/categories/categories-form/categories-form.component.ts
+++ client/src/app/components/categories/categories-form/categories-form.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Category } from './../../../shared/models/categoty';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CategoriesService } from 'src/app/shared/services/categories.service';
 import { of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { MaterialService } from 'src/app/shared/helpers/material.service';
+import { ReadVarExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-categories-form',
@@ -14,6 +16,9 @@ import { MaterialService } from 'src/app/shared/helpers/material.service';
 export class CategoriesFormComponent implements OnInit {
   editMode: boolean = false;
   form: FormGroup;
+  @ViewChild('uploadfile') uploadElem: ElementRef;
+  image: File;
+  imagePreview = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -26,7 +31,7 @@ export class CategoriesFormComponent implements OnInit {
     });
     this.form.disable();
 
-    // HERE IS DINAMIC WAY GETTING DATA FROM PARAMS 
+    // HERE IS DINAMIC WAY GETTING DATA FROM PARAMS
     // this.route.params.subscribe((params: Params) => {
     //   if (params['id']) {
     //     this.editMode = true;
@@ -46,11 +51,12 @@ export class CategoriesFormComponent implements OnInit {
         })
       )
       .subscribe(
-        (data) => {
-          if (data) {
+        (category: Category) => {
+          if (category) {
             this.form.patchValue({
-              name: data.name,
+              name: category.name,
             });
+            this.imagePreview = category.srcUrl;
             MaterialService.reInitTextField();
           }
           this.form.enable();
@@ -58,4 +64,22 @@ export class CategoriesFormComponent implements OnInit {
         (error) => MaterialService.handleError(error.error.message)
       );
   }
+
+  triggerClick() {
+    this.uploadElem.nativeElement.click();
+  }
+
+  onUpload(event: any) {
+    const file = event.target.files[0];
+    this.image = file;
+
+    // here is we uses Filereader for showing preview image 
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSubmit() {}
 }
