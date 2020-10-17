f84fb4dd3e46fce1843db48eb61ff73e8e3bb45e
Valeriy Danets <dantesvv@gmail.com>
2020-10-17 14:47:21 +0300

    added modal window for selected order

--- client/src/app/components/history/history-list/history-list.component.ts
+++ client/src/app/components/history/history-list/history-list.component.ts
@@ -1,22 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+  AfterViewInit,
+  OnDestroy
+} from '@angular/core';
 import { Observable } from 'rxjs';
 import { Order } from 'src/app/shared/models/order';
+import {
+  MaterialService,
+  ModalInterface,
+} from './../../../shared/helpers/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.css'],
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements AfterViewInit, OnDestroy {
   @Input() orders: Order[];
+  @ViewChild('modal') modalRef: ElementRef;
+  modal: ModalInterface;
+  public selectedOrder: Order;
 
-  constructor() {}
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.modalInit(this.modalRef);
+  }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
 
-  computeOrder(order: Order): number {
+  computeTotal(order: Order): number {
     return order.list.reduce((acc, curr) => {
       return (acc += curr.amount * curr.cost);
     }, 0);
+  }
+
+  selectOrder(order: Order): void {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  closeModal(): void {
+    this.modal.close();
   }
 }
