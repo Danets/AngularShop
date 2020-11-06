3163d1c65d42cf954778cd36fefe13038406f604
Valeriy Danets <dantesvv@gmail.com>
2020-11-06 10:27:17 +0200

    added method in service for analytics

--- client/src/app/shared/services/analytics.service.ts
+++ client/src/app/shared/services/analytics.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Overview } from './../models/analytics';
+import { Overview, Analytics } from './../models/analytics';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +11,10 @@ export class AnalyticsService {
 
   getOverview(): Observable<Overview> {
     return this.http.get<Overview>('/api/analytics/overview');
+  }
+
+  getAnalytics(): Observable<Analytics> {
+    return this.http.get<Analytics>('/api/analytics/analytics');
   }
 
 }
