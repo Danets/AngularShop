26e76f3538265535b7b56a4607ce2b2d5f4033df
Danets <dantesvv@gmail.com>
2021-04-12 15:11:37 +0300

    was added logic for CarouselComponent

--- client/src/app/components/ui-components/carousel/carousel.component.ts
+++ client/src/app/components/ui-components/carousel/carousel.component.ts
@@ -6,6 +6,31 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
-  @Input() slides;
+  // @Input() slides;
+
+  public slides = [
+    'https://source.unsplash.com/7BLRSG-AkJs',
+    'https://source.unsplash.com/rcJbbK5_iIA',
+    'https://source.unsplash.com/yQUwIlUeU4o',
+    'https://source.unsplash.com/MlaQmWvzRTw',
+    'https://source.unsplash.com/6dTpYUcr1yg',
+    'https://source.unsplash.com/EyEdZ2aKRfA',
+    'https://source.unsplash.com/qkJ4kHexI2o',
+    'https://source.unsplash.com/XrO6WBflFRo'
+  ];
+
+  currentSlide = 0;
+
+  constructor() {}
+
+  public onPreviousClick(): void {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+  }
+
+  public onNextClick(): void  {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.slides.length ? 0 : next;
+  }
 
 }
